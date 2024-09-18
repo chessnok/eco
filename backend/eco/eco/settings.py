4971a63e9b3bcc9e0ee59dfd5c3d77f09767fcd2
ITQ <itq.dev@ya.ru>
2024-09-19 00:26:13 +0300

    Redesigned frontend

--- backend/eco/eco/settings.py
+++ backend/eco/eco/settings.py
@@ -142,6 +142,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static_dev",
+]
+
+STATIC_ROOT = BASE_DIR / "static"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
